c36bfc34146c99a3b0e4f48db00679dce40e2df1
timzaak <zsy.evan@gmail.com>
2023-11-02 00:32:26 +0800

    bak

--- DriverInstall/src/main.rs
+++ DriverInstall/src/main.rs
@@ -1,3 +1,10 @@
+mod asset;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+    let temp_dir = std::env::temp_dir();
+    //
+    //let data = asset::Asset::get("abc.txt").unwrap();
+    //let data =  std::str::from_utf8(data.data.as_ref());
+    //println!("Hello, world! {data:?}");
 }
